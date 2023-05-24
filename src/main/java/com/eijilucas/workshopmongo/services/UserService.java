b9906082079bf9f461702990734a44fdad640582
Lucas Eiji <104649020+eijilucas@users.noreply.github.com>
2023-05-24 18:58:35 -0300

    exceptions-and-userinsertion

---
+++ src/main/java/com/eijilucas/workshopmongo/services/UserService.java
@@ -0,0 +1,36 @@
+package com.eijilucas.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eijilucas.workshopmongo.dto.UserDTO;
+import com.eijilucas.workshopmongo.entities.User;
+import com.eijilucas.workshopmongo.repositories.UserRepository;
+import com.eijilucas.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+}
